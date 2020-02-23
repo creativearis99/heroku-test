0727cfa4a2388083b576532047ea3e14fdfcb0ce
yurikrupnik <krupnik.yuri@gmail.com>
2020-02-23 17:46:45 +0200

    changed s

--- src/components/routes.js
+++ src/components/routes.js
@@ -1,24 +1,24 @@
 import Loadable from './Loadable';
 
 const Topics = Loadable({
-    loader: () => import(/* webpackChunkName: "topics" */ './Topics'),
+    loader: () => import(/* webpackChunkName: "topics" */ './Topics')
 });
 
 const About = Loadable({
-    loader: () => import(/* webpackChunkName: "about" */ './About'),
+    loader: () => import(/* webpackChunkName: "about" */ './About')
 });
 
 const routes = [
     {
         path: '/topics',
         component: Topics,
-        key: 'topics'
+        key: 'topicsa'
     },
     {
         path: '/about',
         component: About,
         key: 'about'
-    },
+    }
 ];
 
 export default routes;
