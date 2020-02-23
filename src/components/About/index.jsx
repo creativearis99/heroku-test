823838a7da983ff6018a3fe17a1ff4c912ab4607
yurikrupnik <krupnik.yuri@gmail.com>
2020-02-23 23:22:34 +0200

    green

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
+import styles from './about.scss';
 
 const About = () => (
-    <div className="about">
-        <h2>
-            About
-        </h2>
+    <div className={styles.root}>
+        <h2>About ss</h2>
     </div>
 );
 
